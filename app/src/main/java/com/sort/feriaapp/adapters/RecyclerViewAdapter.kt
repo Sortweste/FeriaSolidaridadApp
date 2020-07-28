a48683cac5e999a27c1a7c3e5f0c39e710ee9e3c
Sortweste <00123216@uca.edu.sv>
2020-07-27 23:49:12 -0600

    Basic RecyclerView Adapter Done!

--- app/src/main/java/com/sort/feriaapp/adapters/RecyclerViewAdapter.kt
+++ app/src/main/java/com/sort/feriaapp/adapters/RecyclerViewAdapter.kt
@@ -5,21 +5,21 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.sort.feriaapp.data.Article
-import com.sort.feriaapp.databinding.FragmentHomeBinding
+import com.sort.feriaapp.databinding.CardViewArticlesBinding
 
-class RecyclerViewAdapter(): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
-    private val items = List<Article>
+class RecyclerViewAdapter(private val items: List<Article>): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
-    inner class ViewHolder(val binding: FragmentHomeBinding): RecyclerView.ViewHolder(binding.root)
+    inner class ViewHolder(val binding: CardViewArticlesBinding): RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewAdapter.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding: FragmentHomeBinding = DataBindingUtil.inflate(inflater, viewType, parent, false)
+        val binding: CardViewArticlesBinding = DataBindingUtil.inflate(inflater, viewType, parent, false)
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: RecyclerViewAdapter.ViewHolder, position: Int) {
+        holder.binding.article = items[position]
         holder.binding.executePendingBindings()
     }
 
