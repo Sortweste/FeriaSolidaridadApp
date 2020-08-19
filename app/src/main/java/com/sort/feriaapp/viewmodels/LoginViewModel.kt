e80dbfcee0394ae8cf0e98940f5085c3a8ac5a13
ErickFoxes <37157947+ErickFoxes@users.noreply.github.com>
2020-08-19 20:58:32 -0300

    Regex para correo electronico

--- app/src/main/java/com/sort/feriaapp/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/sort/feriaapp/viewmodels/LoginViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.sort.feriaapp.data.repositories.UserRepository
 import com.sort.feriaapp.network.dtos.LoginDTO
+import java.util.regex.Pattern
 
 
 class LoginViewModel @ViewModelInject constructor(private val repository: UserRepository): ViewModel(){
@@ -14,25 +15,34 @@ class LoginViewModel @ViewModelInject constructor(private val repository: UserRe
     val inputPassword = MutableLiveData<String>()
     val errorEmail = MutableLiveData<String>()
     val errorPassword = MutableLiveData<String>()
-
+    val emailPattern = Regex("^[\\w-\\.]+@((uca)+\\.)+((edu)\\.)+(sv)\$")
     fun login() = repository.login(LoginDTO(inputEmail.value.toString(), inputPassword.value.toString()))
 
     fun performValidation(): Boolean{
-        if(inputEmail.value.isNullOrBlank()) {
-            errorEmail.value = "Ingrese correo electrónico"
+
+        if(inputEmail.value.isNullOrBlank() || inputPassword.value.isNullOrBlank() || !inputEmail.value.toString().matches(emailPattern)){
+
+            if(inputEmail.value.isNullOrBlank()) {
+                errorEmail.value = "Ingrese correo electrónico"
+            }else if(!inputEmail.value.toString().matches(emailPattern)) {
+                errorEmail.value = "Ingrese un correo con extensión @uca.edu.sv"
+            }else{
+                errorEmail.value = null
+            }
+
+            if(inputPassword.value.isNullOrBlank()) {
+                errorPassword.value = "Ingrese contraseña"
+            }else{
+                errorPassword.value = null
+            }
+
             return false
-        }
-        else{
+
+        }else {
             errorEmail.value = null
-        }
-        if(inputPassword.value.isNullOrBlank()) {
-            errorPassword.value = "Ingrese contraseña"
-            return false
-        }
-        else {
             errorPassword.value = null
         }
-        //if(!inputEmail.value.isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher().matches())
+
         return true
     }
 
