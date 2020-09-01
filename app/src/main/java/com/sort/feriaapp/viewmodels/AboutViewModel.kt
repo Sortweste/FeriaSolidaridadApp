39d6ad302b661596c01e3343035a7c3ed831bd0a
Sortweste <00123216@uca.edu.sv>
2020-08-31 22:27:12 -0600

    Added Oustanding layout!

---
+++ app/src/main/java/com/sort/feriaapp/viewmodels/AboutViewModel.kt
@@ -0,0 +1,10 @@
+package com.sort.feriaapp.viewmodels
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class AboutViewModel @ViewModelInject constructor(): ViewModel() {
+
+    val data = "Este año celebramos la XIX feria de la solidaridad cuyo slogan es: \"Conectados transformamos realidades\", una actividad que busca siempre animar la participación de nuestros estudiantes en la realización de su servicio social, atendiendo las necesidades de sectores vulnerables de la población. En estos tiempos de pandemia en que el aislamiento físico forzado impera en nuestra sociedad es importante no perder nunca la empatía y la solidaridad con quienes más lo necesitan. Desde el inicio de marzo hasta la fecha nuestros alumnos han continuado desarrollando el servicio social pese a la crisis producida por el COVID-19. Nos ha tocado reinventarnos y buscar alternativas de servicio social acordes a la realidad actual del país. Es a través de esta aplicación que queremos seguir compartiendo con ustedes nuestra misión de transformar la realidad desde el conocimiento y la cercanía solidaria de nuestros estudiantes con las mayorías populares de este país."
+
+}
